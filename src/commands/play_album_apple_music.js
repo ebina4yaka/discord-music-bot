e947f4423cf43a1c97d71edb15d6f7e852da8b41
ebina4yaka <ebina4yaka@protonmail.com>
2024-05-15 07:22:42 +0900

    chore: change descripion

--- src/commands/play_album_apple_music.js
+++ src/commands/play_album_apple_music.js
@@ -5,7 +5,7 @@ const addMultipleTracks = require('../utils/addMultipleTracks')
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play_album_apple_music')
-    .setDescription('音楽を再生します')
+    .setDescription('アルバムを再生します')
     .addStringOption((option) =>
       option.setName('url').setDescription('音楽のURL').setRequired(true),
     ),
