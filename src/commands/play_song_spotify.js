4ee88f5ab840fc61c55ea8bc9e08338b5545b9f2
shunsuke takii <ebina4yaka@protonmail.com>
2024-05-15 17:27:27 +0900

    fix: fix args

--- src/commands/play_song_spotify.js
+++ src/commands/play_song_spotify.js
@@ -11,6 +11,6 @@ module.exports = {
     ),
 
   run: async ({ client, interaction }) => {
-    await addSingleTrack(client, interaction, QueryType.SPOTIFY_PLAYLIST)
+    await addSingleTrack(client, interaction, QueryType.SPOTIFY_SONG)
   },
 }
