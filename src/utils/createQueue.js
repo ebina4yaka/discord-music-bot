e9654e2c695ee1c798ee3c9fd6fa0b76ff08af2b
ebina4yaka <ebina4yaka@protonmail.com>
2024-05-16 04:10:58 +0900

    fix: fix metadata

--- src/utils/createQueue.js
+++ src/utils/createQueue.js
@@ -1,13 +1,14 @@
 module.exports = (client, interaction) => {
   return client.player.nodes.create(interaction.guild, {
-    metadata: interaction.channel,
-    bufferingTimeout: 15000, //How long the player should attempt buffering before giving up
-    leaveOnStop: true, //If player should leave the voice channel after user stops the player
-    leaveOnStopCooldown: 5000, //Cooldown in ms
-    leaveOnEnd: true, //If player should leave after the whole queue is over
-    leaveOnEndCooldown: 15000, //Cooldown in ms
-    leaveOnEmpty: true, //If the player should leave when the voice channel is empty
-    leaveOnEmptyCooldown: 300000, //Cooldown in ms
-    skipOnNoStream: true,
+    metadata: interaction.metadata,
+    noEmitInsert: true,
+    leaveOnStop: false,
+    leaveOnEmpty: true,
+    leaveOnEmptyCooldown: 60000,
+    leaveOnEnd: true,
+    leaveOnEndCooldown: 60000,
+    pauseOnEmpty: true,
+    preferBridgedMetadata: true,
+    disableBiquad: true,
   })
 }
